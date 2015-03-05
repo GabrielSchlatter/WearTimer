d782dee47c64c3df33995b53932c786cb8a00fa4
Dmytro Khmelenko <dmytro@mobilabsolutions.com>
2015-03-05 18:22:09 +0100

    Mobile project added

---
+++ mobile/src/main/java/com/cologne/hackaton/wearstopwatch/LapAdapter.java
@@ -0,0 +1,53 @@
+package com.cologne.hackaton.wearstopwatch;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.gabriel.android.timelib.model.Lap;
+import com.gabriel.android.timelib.utils.StringUtils;
+
+import java.util.List;
+
+/**
+ * Provides adapter class for the list of Lap
+ *
+ * @author Dmytro Khmelenko, Gabriel Schlatter
+ */
+public class LapAdapter extends ArrayAdapter<Lap> {
+
+    private Context mContext;
+
+    public LapAdapter(Context context, int resource, List<Lap> objects) {
+        super(context, resource, objects);
+        mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.lap_list_item, null);
+            LapHolder holder = new LapHolder();
+            holder.mText = (TextView) convertView.findViewById(R.id.tv_text);
+            convertView.setTag(holder);
+        }
+
+        LapHolder holder = (LapHolder) convertView.getTag();
+        Lap lap = getItem(position);
+        String lapString = "# " + lap.getLapNumber() + " "
+                + StringUtils.formatString(lap.getLapTime()) + " - "
+                + StringUtils.formatString(lap.getTimeSum());
+        holder.mText.setText(lapString);
+        return convertView;
+    }
+
+    private class LapHolder {
+        private TextView mText;
+    }
+}
--- wear/src/main/java/com/cologne/hackaton/wearstopwatch/activity/stopwatch/StopWatchActivity.java
+++ wear/src/main/java/com/cologne/hackaton/wearstopwatch/activity/stopwatch/StopWatchActivity.java
@@ -11,16 +11,33 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cologne.hackaton.wearstopwatch.R;
 import com.gabriel.android.timelib.StopWatch;
 import com.gabriel.android.timelib.model.Lap;
 import com.gabriel.android.timelib.utils.StringUtils;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
 /**
  * @author Dmytro Khmelenko, Gabriel Schlatter
  */
-public class StopWatchActivity extends Activity {
+public class StopWatchActivity extends Activity implements DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, MessageApi.MessageListener {
+
+    private static final String START_STOPWATCH_PATH = "start/stopwatch";
+    private static final String SAVE_LAP_MESSAGE = "save/laptime";
+    private static final String RESET_STOPWATCH = "reset/watch";
+    private static final String PAUSE_WATCH = "pause/watch";
 
   private StopWatch stopWatch;
 
@@ -37,10 +54,18 @@ public class StopWatchActivity extends Activity {
   private List<Lap> mLaps = new ArrayList<>();
   private LapAdapter mLapsAdapter;
 
+    // Wear Api
+    private GoogleApiClient mGoogleApiClient;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_stopwatch);
+
+      // Create a GoogleApiClient instance
+      mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Wearable.API)
+              .addConnectionCallbacks(this).addOnConnectionFailedListener(this)
+              .build();
 
     final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
     stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -65,11 +90,29 @@ public class StopWatchActivity extends Activity {
               mLaps.clear();
             }
             mLapsAdapter.notifyDataSetChanged();
+
+              // TODO
+             // sendMessage(SAVE_LAP_MESSAGE, Long.toString(lapTime));
           }
         });
       }
     });
   }
+
+    private void sendMessage(final String path, final String text) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi
+                        .getConnectedNodes(mGoogleApiClient).await();
+                for (Node node : nodes.getNodes()) {
+                    MessageApi.SendMessageResult result = Wearable.MessageApi
+                            .sendMessage(mGoogleApiClient, node.getId(), path,
+                                    text != null ? text.getBytes() : null).await();
+                }
+            }
+        }).start();
+    }
 
   /**
    * Initializes view controls
@@ -184,4 +227,37 @@ public class StopWatchActivity extends Activity {
       mLocked = false;
     }
   }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(getClass().getSimpleName(), "onConnected: " + bundle);
+        Toast.makeText(StopWatchActivity.this, "Connected to Data Layer API",
+                Toast.LENGTH_SHORT).show();
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(getClass().getSimpleName(), "onConnectionSuspended: " + i);
+        Toast.makeText(StopWatchActivity.this, "Connectio to Data ayer API suspended",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(getClass().getSimpleName(), "onConnectionFailed: " + connectionResult);
+        Toast.makeText(StopWatchActivity.this, "Connection to Data Layer API failed",
+                Toast.LENGTH_SHORT).show();
+    }
 }
